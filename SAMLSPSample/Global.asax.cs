1e5a9f4d340a88e2d874582c76f5be7106d6b19f
Ramesh Kumar <Avula.Ramesh.Kumar@FISGLOBAL.COM>
2023-12-13 19:25:01 +0530

    initial commit 13Dec2023

---
+++ SAMLSPSample/Global.asax.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+using ComponentSpace.SAML2.Notifications;
+using ComponentSpace.SAML2.Protocols;
+using SAMLSPSample.SAMLSP;
+
+namespace SAMLSPSample
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            SAMLObservable.Subscribe(new SAMLObserver());
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                string s =HttpContext.Current.Request.Headers.ToString();
+                System.Diagnostics.Debug.WriteLine("args1: {0} ", s);
+                HttpContext.Current.Request.Headers["Accept-Encoding"] = "";
+                HttpContext.Current.Request.Headers.Remove("Accept-Encoding");
+                HttpContext.Current.Response.Headers.Remove("Accept-Encoding");
+                s = HttpContext.Current.Request.Headers.ToString();
+                System.Diagnostics.Debug.WriteLine("args2: {0} ", s);
+
+            }
+            catch (Exception) { }
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                string cookieName = FormsAuthentication.FormsCookieName;
+                string cookieVal;
+                HttpCookie authCookie;
+                // extract the FormsAuthentication cookie
+                if (HttpContext.Current.Request.IsAuthenticated) { }
+                authCookie = HttpContext.Current.Request.Cookies[cookieName];
+                if (authCookie == null)
+                {
+                    if (HttpContext.Current.Request.QueryString.Get("Auth") == null)
+                    {
+                        if ((HttpContext.Current.Session == null) || (HttpContext.Current.Session.Contents["AuthCookie"] == null))
+                            // no authorization cookie is found
+                            return;
+                        cookieVal = Convert.ToString(HttpContext.Current.Session.Contents["AuthCookie"]);
+                    }
+                    else
+                        cookieVal = HttpContext.Current.Request.QueryString.Get("Auth");
+                    authCookie = new HttpCookie("cookiename", cookieVal);
+                }
+                FormsAuthenticationTicket tkt;
+                try
+                {
+                    tkt = FormsAuthentication.Decrypt(authCookie.Value);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                // has the ticket expired ??
+                if (tkt.Expired)
+                    return;
+                //parse list of permissions
+                string[] roles;
+                FormsAuthentication.RenewTicketIfOld(tkt);
+                roles = tkt.UserData.Split(new char[] { ',' });
+                // create identity object
+                FormsIdentity id = new FormsIdentity(tkt);
+                //this principal will flow through the request
+                GenericPrincipal principal = new GenericPrincipal(id, roles);
+                //attach the new principal to the current HttpContext object
+                HttpContext.Current.User = principal;
+            }
+            catch (Exception ex) { 
+                
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SAMLSPSample/Scripts/WebForms/Default.aspx.cs
@@ -0,0 +1,91 @@
+﻿using ComponentSpace.SAML2.Exceptions;
+using ComponentSpace.SAML2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SAMLSPSample.Scripts.WebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IDictionary<string, string> attributes = (IDictionary<string, string>)Session[SAMLSP.AssertionConsumerService.AttributesSessionKey];
+            if (attributes == null)
+            {
+                return;
+            }
+            string htmlTable = "<table id='myTable' style='border: 1px solid black;' > <tr class='header'><th style = 'width:20%;'> Attribute </th><th style = 'width:40%;'> Value </th> </tr>";
+            HttpCookie ramesauthcookie = new HttpCookie("myrameshcokieguid");
+
+            foreach (KeyValuePair<string, string> entry in attributes)
+            {
+                // do something with entry.Value or entry.Key
+                if (entry.Key == "guid")
+                {
+                    ramesauthcookie.Value = entry.Value;
+                    Response.Cookies[entry.Key].Value = entry.Value;
+                }
+                if (entry.Key == "ot_cookie_consent_token")
+                {
+                    ramesauthcookie.Value = entry.Value;
+                    Response.Cookies[entry.Key].Value = entry.Value;
+                }
+                // do something with entry.Value or entry.Key
+                htmlTable += "<tr><td>" + entry.Key + "</td><td>" + entry.Value + "</td></tr>";
+            }
+            //First get user claims    
+            var claims = ClaimsPrincipal.Current.Identities.First().Claims.ToList();
+            var sesSionOj = HttpContext.Current.Session;
+            var req = (User.Identity as ClaimsIdentity).Claims.Where(c => c.Type == "FirmName").FirstOrDefault();
+           // string reqValue = req.Value;
+            //Filter specific claim    
+            //  claims?.FirstOrDefault(x => x.Type.Equals("FirmName", StringComparison.OrdinalIgnoreCase))?.Value
+            htmlTable += "<tr><td>"+ " " +  "</td></tr>";
+
+            htmlTable += "</table>";
+            lblTable.Text = htmlTable;
+        }
+
+
+
+        protected void logoutButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Logout locally.
+                FormsAuthentication.SignOut();
+
+                if (SAMLServiceProvider.CanSLO(WebConfigurationManager.AppSettings[AppSettings.PartnerIdP]))
+                {
+                    // Request logout at the identity provider.
+                    string partnerIdP = WebConfigurationManager.AppSettings[AppSettings.PartnerIdP];
+                    SAMLServiceProvider.InitiateSLO(Response, null, null, partnerIdP);
+
+                    
+                }
+                else
+                {
+                    
+                    Response.Redirect("../../../loginSP.aspx");
+
+                }
+            }
+            catch (SAMLConfigurationException samlconfigex)
+            {
+                throw samlconfigex;
+            }
+            catch (SAMLException samlex)
+            {
+                throw samlex;
+            }
+
+        }
+    }
+}
